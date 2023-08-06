1193646c182414c31cc5759b4cf31ab111e9b373
jinyeying <36725804+jinyeying@users.noreply.github.com>
2023-08-06 15:49:50 -0700

    Add files via upload

---
+++ 0_ACMMM23_RESULTS/calculate_psnr_ssim_NH_GTA5.py
@@ -0,0 +1,255 @@
+import os
+import cv2
+import torch
+import numpy as np
+# This script is adapted from the following repository: https://github.com/JingyunLiang/SwinIR
+
+def calculate_psnr(img1, img2, test_y_channel=False):
+    """Calculate PSNR (Peak Signal-to-Noise Ratio).
+
+    Ref: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+
+    Args:
+        img1 (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: psnr result.
+    """
+
+    assert img1.shape == img2.shape, (f'Image shapes are differnet: {img1.shape}, {img2.shape}.')
+    assert img1.shape[2] == 3
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+
+    if test_y_channel:
+        img1 = to_y_channel(img1)
+        img2 = to_y_channel(img2)
+
+    mse = np.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return float('inf')
+    return 20. * np.log10(255. / np.sqrt(mse))
+
+def _ssim(img1, img2):
+    """Calculate SSIM (structural similarity) for one channel images.
+
+    It is called by func:`calculate_ssim`.
+
+    Args:
+        img1 (ndarray): Images with range [0, 255] with order 'HWC'.
+        img2 (ndarray): Images with range [0, 255] with order 'HWC'.
+
+    Returns:
+        float: ssim result.
+    """
+
+    C1 = (0.01 * 255) ** 2
+    C2 = (0.03 * 255) ** 2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1 ** 2
+    mu2_sq = mu2 ** 2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+def calculate_ssim(img1, img2, test_y_channel=False):
+    """Calculate SSIM (structural similarity).
+
+    Ref:
+    Image quality assessment: From error visibility to structural similarity
+
+    The results are the same as that of the official released MATLAB code in
+    https://ece.uwaterloo.ca/~z70wang/research/ssim/.
+
+    For three-channel images, SSIM is calculated for each channel and then
+    averaged.
+
+    Args:
+        img1 (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: ssim result.
+    """
+
+    assert img1.shape == img2.shape, (f'Image shapes are differnet: {img1.shape}, {img2.shape}.')
+    assert img1.shape[2] == 3
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+
+    if test_y_channel:
+        img1 = to_y_channel(img1)
+        img2 = to_y_channel(img2)
+
+    ssims = []
+    for i in range(img1.shape[2]):
+        ssims.append(_ssim(img1[..., i], img2[..., i]))
+    return np.array(ssims).mean()
+
+def to_y_channel(img):
+    """Change to Y channel of YCbCr.
+
+    Args:
+        img (ndarray): Images with range [0, 255].
+
+    Returns:
+        (ndarray): Images with range [0, 255] (float type) without round.
+    """
+    img = img.astype(np.float32) / 255.
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = bgr2ycbcr(img, y_only=True)
+        img = img[..., None]
+    return img * 255.
+
+def _convert_input_type_range(img):
+    """Convert the type and range of the input image.
+
+    It converts the input image to np.float32 type and range of [0, 1].
+    It is mainly used for pre-processing the input image in colorspace
+    convertion functions such as rgb2ycbcr and ycbcr2rgb.
+
+    Args:
+        img (ndarray): The input image. It accepts:
+            1. np.uint8 type with range [0, 255];
+            2. np.float32 type with range [0, 1].
+
+    Returns:
+        (ndarray): The converted image with type of np.float32 and range of
+            [0, 1].
+    """
+    img_type = img.dtype
+    img = img.astype(np.float32)
+    if img_type == np.float32:
+        pass
+    elif img_type == np.uint8:
+        img /= 255.
+    else:
+        raise TypeError('The img type should be np.float32 or np.uint8, ' f'but got {img_type}')
+    return img
+
+def _convert_output_type_range(img, dst_type):
+    """Convert the type and range of the image according to dst_type.
+
+    It converts the image to desired type and range. If `dst_type` is np.uint8,
+    images will be converted to np.uint8 type with range [0, 255]. If
+    `dst_type` is np.float32, it converts the image to np.float32 type with
+    range [0, 1].
+    It is mainly used for post-processing images in colorspace convertion
+    functions such as rgb2ycbcr and ycbcr2rgb.
+
+    Args:
+        img (ndarray): The image to be converted with np.float32 type and
+            range [0, 255].
+        dst_type (np.uint8 | np.float32): If dst_type is np.uint8, it
+            converts the image to np.uint8 type with range [0, 255]. If
+            dst_type is np.float32, it converts the image to np.float32 type
+            with range [0, 1].
+
+    Returns:
+        (ndarray): The converted image with desired type and range.
+    """
+    if dst_type not in (np.uint8, np.float32):
+        raise TypeError('The dst_type should be np.float32 or np.uint8, ' f'but got {dst_type}')
+    if dst_type == np.uint8:
+        img = img.round()
+    else:
+        img /= 255.
+    return img.astype(dst_type)
+
+def bgr2ycbcr(img, y_only=False):
+    """Convert a BGR image to YCbCr image.
+
+    The bgr version of rgb2ycbcr.
+    It implements the ITU-R BT.601 conversion for standard-definition
+    television. See more details in
+    https://en.wikipedia.org/wiki/YCbCr#ITU-R_BT.601_conversion.
+
+    It differs from a similar function in cv2.cvtColor: `BGR <-> YCrCb`.
+    In OpenCV, it implements a JPEG conversion. See more details in
+    https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion.
+
+    Args:
+        img (ndarray): The input image. It accepts:
+            1. np.uint8 type with range [0, 255];
+            2. np.float32 type with range [0, 1].
+        y_only (bool): Whether to only return Y channel. Default: False.
+
+    Returns:
+        ndarray: The converted YCbCr image. The output image has the same type
+            and range as input image.
+    """
+    img_type = img.dtype
+    img = _convert_input_type_range(img)
+    if y_only:
+        out_img = np.dot(img, [24.966, 128.553, 65.481]) + 16.0
+    else:
+        out_img = np.matmul(
+            img, [[24.966, 112.0, -18.214], [128.553, -74.203, -93.786], [65.481, -37.797, 112.0]]) + [16, 128, 128]
+    out_img = _convert_output_type_range(out_img, img_type)
+    return out_img
+
+base_path = '/Users/yeyingjin/Dropbox/nighttime/ACMMM23/0_ACMMM23_RESULTS/'
+
+# dataset_name = 'NHM'
+# dataset_name = 'NHR'
+# dataset_name = 'NHC'
+dataset_name = 'GTA5'
+
+gt_path      = base_path + dataset_name + '/img_1/'
+results_path = base_path + dataset_name + '/img_2/'
+
+imgslist = []
+
+dir_1 = sorted(os.listdir(results_path))
+for j in range(len(dir_1)):
+    imgslist.append(results_path +dir_1[j])
+print('-len(imgslist)-',len(imgslist))
+gtslist = []
+for i in range(len(imgslist)):
+    gts = imgslist[i].replace(results_path,gt_path)
+    gtslist.append(gts)
+print(gtslist[0])
+print(gtslist[-1])
+print(imgslist[0])
+print(imgslist[-1])
+print('-len(imgslist)-',len(imgslist),len(gtslist))
+cumulative_psnr, cumulative_ssim = 0, 0
+count= 0 
+
+for i in range(len(imgslist)):
+    res = cv2.imread(imgslist[i], cv2.IMREAD_COLOR)
+    gt  = cv2.imread(gtslist[i], cv2.IMREAD_COLOR)
+    if i%100==0:
+        print('current i:',i)
+    if res.shape == gt.shape:
+        count = count + 1
+        cur_psnr = calculate_psnr(res, gt, test_y_channel=True)
+        cur_ssim = calculate_ssim(res, gt, test_y_channel=True)
+        #cur_psnr = calculate_psnr(res, gt, test_y_channel=False)
+        #cur_ssim = calculate_ssim(res, gt, test_y_channel=False)        
+
+        cumulative_psnr += cur_psnr
+        cumulative_ssim += cur_ssim
+        if count%100==0:
+            print('Testing set,'+str(i)+' PSNR is %.4f, SSIM is %.4f ' % (cumulative_psnr / (count), cumulative_ssim / (count)))
+print('Testing set, PSNR is %.4f, SSIM is %.4f ' % (cumulative_psnr / count, cumulative_ssim / count), count)
+print(results_path)
+
+#NHR, PSNR is 27.3292, SSIM is 0.9328  897
+#NHM, PSNR is 34.7331, SSIM is 0.9420  350
+#NHC, PSNR is 39.2277, SSIM is 0.9765  550
+#GTA5, PSNR is 30.5298, SSIM is 0.9060  77
